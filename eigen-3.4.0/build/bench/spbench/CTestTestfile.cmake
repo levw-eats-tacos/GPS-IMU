7fa24a02d232b3d9425f81298476e8c94fe02ac1
Evan Ryan <eryan@insight.rr.com>
2022-10-31 14:03:43 -0400

    Delete eigen-3.4.0/build directory

--- eigen-3.4.0/build/bench/spbench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/udayton/Desktop/GPS-IMU/GPS-IMU/eigen-3.4.0/bench/spbench
-# Build directory: /home/udayton/Desktop/GPS-IMU/GPS-IMU/eigen-3.4.0/build/bench/spbench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- eigen-3.4.0/build/scripts/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/udayton/Desktop/GPS-IMU/GPS-IMU/eigen-3.4.0/scripts
-# Build directory: /home/udayton/Desktop/GPS-IMU/GPS-IMU/eigen-3.4.0/build/scripts
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
