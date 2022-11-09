3d041459ce2d28d3ccd93287eccee559bc4e567d
davisoni1 <davisoni1@udayton.edu>
2022-11-09 13:21:46 -0500

    changed gpstest for easy read

--- BerryIMU/gpstest.py
+++ BerryIMU/gpstest.py
@@ -10,14 +10,7 @@ try:
         report = gpsd.next() #
         if report['class'] == 'TPV':
              
-            print(getattr(report,'lat',0.0),"\t"),
-            print(getattr(report,'lon',0.0),"\t"),
-            print(getattr(report,'time',''),"\t"),
-            print(getattr(report,'alt','nan'),"\t\t"),
-            print(getattr(report,'epv','nan'),"\t"),
-            print(getattr(report,'ept','nan'),"\t"),
-            print(getattr(report,'speed','nan'),"\t"),
-            print(getattr(report,'climb','nan'),"\t")
+            print(getattr(report,'lat',0.0)+"\t"+getattr(report,'lon',0.0)+"\t"+getattr(report,'time','')+"\t"+getattr(report,'alt','nan')+"\t\t"+getattr(report,'epv','nan')+"\t"+getattr(report,'ept','nan')+"\t"+getattr(report,'speed','nan')+"\t"+ getattr(report,'climb','nan')+"\t")
         time.sleep(1) 
 except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
     print("Done.\nExiting.")
