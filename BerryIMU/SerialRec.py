24c9076e63e2b28e1fe2e51b987a6e4dead831a2
davisoni1 <davisoni1@udayton.edu>
2022-11-14 13:43:15 -0500

    changes

--- BerryIMU/SerialRec.py
+++ BerryIMU/SerialRec.py
@@ -5,8 +5,12 @@ s = serial.Serial("COM8")
 s.flushInput()
 s.flushOutput()
 
+s.write('1'.encode('utf-8'))
+
 while True:
     bytesToRead = s.inWaiting()
-    help = s.read(bytesToRead)
-    decoded = help.decode('utf-8', 'ignore')
-    print(decoded)
+    if(bytesToRead > 35):
+        help = s.read(bytesToRead)
+        decoded = help.decode('utf-8', 'ignore')
+        print(decoded)
+        s.write('1'.encode('utf-8'))
--- BerryIMU/SerialSend.py
+++ BerryIMU/SerialSend.py
@@ -12,8 +12,10 @@ class SerialWrapper:
 def main():
     ser = serial.Serial('/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_CTBHb116L16-if00-port0',9600)
     while True:
-        data = 'this is a really long scentence plox'
-        ser.write(data.encode('utf-8'))
+        if(ser.in_waiting > 0):
+            data = 'this is a really long scentence plox'
+            ser.write(data.encode('utf-8'))
+            ser.reset_input_buffer()
 
 
 main()
