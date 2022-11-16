2c080caa8c198468b523d1b243fe7bf49182010d
davisoni1 <davisoni1@udayton.edu>
2022-11-16 13:48:23 -0500

    Fixed Packet Loss Bug

--- BerryIMU/Echo_client.py
+++ BerryIMU/Echo_client.py
@@ -9,8 +9,10 @@ import json
 HOST = "127.0.0.1"
 PORT = 65432
 
+ser = serial.Serial('/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_CTBHb116L16-if00-port0',9600)
+
 def serialOut(data):
-	ser = serial.Serial('/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_CTBHb116L16-if00-port0',9600)
+	global ser
 	if(ser.in_waiting > 0):
 		jason = json.dumps(data)
 		ser.write(jason.encode('utf-8'))
@@ -28,6 +30,7 @@ def socketRec():
 		s.connect((HOST,PORT))
 		s.sendall(b"Hello,world")
 		data = s.recv(1024)
+		s.close()
 	#print(f"Received {data!r}")
 	return data
 
@@ -47,16 +50,16 @@ try:
 		serialOut(r)
 		print(r)
 		#GPS Data
-		if report['class'] == 'TPV':
+		# if report['class'] == 'TPV':
              
-			print(getattr(report,'lat',0.0),"\t"),
-			print(getattr(report,'lon',0.0),"\t"),
-			print(getattr(report,'time',''),"\t"),
-			print(getattr(report,'alt','nan'),"\t\t"),
-			print(getattr(report,'epv','nan'),"\t"),
-			print(getattr(report,'ept','nan'),"\t"),
-			print(getattr(report,'speed','nan'),"\t"),
-			print(getattr(report,'climb','nan'),"\t")
+		# 	print(getattr(report,'lat',0.0),"\t"),
+		# 	print(getattr(report,'lon',0.0),"\t"),
+		# 	print(getattr(report,'time',''),"\t"),
+		# 	print(getattr(report,'alt','nan'),"\t\t"),
+		# 	print(getattr(report,'epv','nan'),"\t"),
+		# 	print(getattr(report,'ept','nan'),"\t"),
+		# 	print(getattr(report,'speed','nan'),"\t"),
+		# 	print(getattr(report,'climb','nan'),"\t")
 		#ser.write("test test") replace with JSON when ready.
 		time.sleep(1)
 except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
--- BerryIMU/SerialRec.py
+++ BerryIMU/SerialRec.py
@@ -5,15 +5,21 @@ s = serial.Serial("COM8")
 
 s.flushInput()
 s.flushOutput()
-
+x=0
 s.write('1'.encode('utf-8'))
 
 while True:
     bytesToRead = s.inWaiting()
     print(bytesToRead)
-    if(bytesToRead > 40):
+    x=x+1
+    if(x>1):
+        s.write('1'.encode('utf-8'))
+    if(bytesToRead > 0):
+        x = 0
+    if(bytesToRead > 26):
         help = s.read(bytesToRead)
         decoded = help.decode('utf-8', 'ignore')
         print(decoded)
         s.write('1'.encode('utf-8'))
-        time.sleep(1)
+        time.sleep(10)
+        x= 0
