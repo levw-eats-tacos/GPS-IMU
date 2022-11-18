0e1ce40f6e7c625a7a4b5eb5723415dfc9ccb7c8
davisoni1 <davisoni1@udayton.edu>
2022-11-18 13:25:20 -0500

    Finished SerialRec.py. Code should be all up to date.

--- BerryIMU/SerialRec.py
+++ BerryIMU/SerialRec.py
@@ -12,17 +12,18 @@ s.write('1'.encode('utf-8'))
 def buffercheck():
     global s
     stringOfStuff = ""
-    nextcharByte = s.read(1)
-    nextchar = nextcharByte.decode('utf-8', 'ignore')
-    if(nextchar == "}"):
-        stringOfStuff = stringOfStuff + nextchar
-        return stringOfStuff
-    else:
-        stringOfStuff = stringOfStuff + nextchar
+    while(True):
+        nextcharByte = s.read(1)
+        nextchar = nextcharByte.decode('utf-8', 'ignore')
+        if(nextchar == "}"):
+            stringOfStuff = stringOfStuff + nextchar
+            return stringOfStuff
+        else:
+            stringOfStuff = stringOfStuff + nextchar
+
 
 while True:
     bytesToRead = s.inWaiting()
-    print(bytesToRead)
     x=x+1
     if(x>1):
         s.write('1'.encode('utf-8'))
@@ -30,7 +31,7 @@ while True:
         x = 0
     #format decimal places so we dont have a caniption
     #if(bytesToRead > 154):
-        #help = s.readline()
+        #help = s.read(bytesToRead)
         #decoded = help.decode('utf-8', 'ignore')
         #print(decoded)
         #s.write('1'.encode('utf-8'))
@@ -39,4 +40,4 @@ while True:
         output = buffercheck()
         print(output)
         s.write('1'.encode('utf-8'))
-        time.sleep(1)
+        time.sleep(.25)
