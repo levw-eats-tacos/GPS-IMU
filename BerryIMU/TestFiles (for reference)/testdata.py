9922505a83f39058bda6fcc4ba241e309685a181
jackkoenig1 <koenigj5@udayton.edu>
2022-11-30 13:48:31 -0500

    adding testing script to plot imu data and PRY to xyz

--- BerryIMU/TestFiles (for reference)/testdata.py
+++ BerryIMU/TestFiles (for reference)/testdata.py
@@ -1,9 +1,47 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import math
+## new code
 
-soa = np.array([[0, 0, 1, 1, -2, 0], [0, 0, 2, 1, 1, 0],
-                [0, 0, 3, 2, 1, 0], [0, 0, 4, 0.5, 0.7, 0]])
+
+roll = 2.6335
+pitch = 0.4506
+yaw = 1.1684
+
+
+yawMatrix = np.matrix([
+[math.cos(yaw), -math.sin(yaw), 0],
+[math.sin(yaw), math.cos(yaw), 0],
+[0, 0, 1]
+])
+
+pitchMatrix = np.matrix([
+[math.cos(pitch), 0, math.sin(pitch)],
+[0, 1, 0],
+[-math.sin(pitch), 0, math.cos(pitch)]
+])
+
+rollMatrix = np.matrix([
+[1, 0, 0],
+[0, math.cos(roll), -math.sin(roll)],
+[0, math.sin(roll), math.cos(roll)]
+])
+
+R = yawMatrix * pitchMatrix * rollMatrix
+
+theta = math.acos(((R[0, 0] + R[1, 1] + R[2, 2]) - 1) / 2)
+multi = 1 / (2 * math.sin(theta))
+
+rx = multi * (R[2, 1] - R[1, 2]) * theta
+ry = multi * (R[0, 2] - R[2, 0]) * theta
+rz = multi * (R[1, 0] - R[0, 1]) * theta
+
+##
+
+
+
+soa = np.array([0, 0, 1, 1, -2, 0])
 
 X, Y, Z, U, V, W = zip(*soa)
 fig = plt.figure()
