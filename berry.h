32514d8c765c03e218e89554fe1da6f0f22f17d1
levw-eats-tacos <eryan@insight.rr.com>
2022-12-05 14:14:55 -0500

    Adding code to interface with test code

---
+++ berry.h
@@ -0,0 +1,31 @@
+#ifndef BERRY_H
+#define BERRY_H
+
+#include "device.h"
+/*
+    The Device class is an interface used for the GPS/IMU
+    devices that will be tested. If any device needs to be
+    tested, one only needs to make a subclass of this and
+    inherit the
+*/
+class Berry:public Device{
+public slots:
+
+    // function for updating the ypr instance variable
+    virtual void updateYPR(){}
+
+    // function for updating the gps instance variable
+    virtual void updateGPS(){}
+public:
+    // constructor
+    Berry():Device(){}
+
+    // function for connecting to the device
+    virtual void connect(){}
+
+    // function for disconnecting the device
+    virtual void disconnect(){}
+
+};
+
+#endif // BERRY_H
---
+++ device.h
@@ -0,0 +1,47 @@
+#ifndef DEVICE_H
+#define DEVICE_H
+
+#include <QObject>
+#include <windows.h>
+
+/*
+    The Device class is an interface used for the GPS/IMU
+    devices that will be tested. If any device needs to be
+    tested, one only needs to make a subclass of this and
+    inherit the
+*/
+class Device:public QObject{
+public slots:
+
+    // function for updating the ypr instance variable
+    virtual void updateYPR();
+
+    // function for updating the gps instance variable
+    virtual void updateGPS();
+public:
+    // constructor
+    Device():QObject();
+
+    // function for connecting to the device
+    virtual void connect();
+
+    // function for disconnecting the device
+    virtual void disconnect();
+
+    // getter function for ypr
+    virtual float *getArr(){
+        return arr;
+    }
+
+
+protected:
+    // stores the current yaw, pitch, and roll of the device
+    float arr[3];
+
+private:
+    HANDLE m_serialHandle;
+
+
+};
+
+#endif // DEVICE_H
